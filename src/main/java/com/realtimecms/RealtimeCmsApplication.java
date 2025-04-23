a0a8d0e1c986884a5468cf5a8cef14af48b74b54
practicaluser <dlcnsdncjswo@naver.com>
2025-04-23 22:18:16 +0900

    Complete Sprint 1 Stage 1: Update README and add PostgreSQL connection

---
+++ src/main/java/com/realtimecms/RealtimeCmsApplication.java
@@ -0,0 +1,13 @@
+package com.realtimecms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/realtimecms/RealtimeCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realtimecms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
